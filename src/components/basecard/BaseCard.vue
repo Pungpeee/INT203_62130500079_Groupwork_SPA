9380ad341fa95e350234d08989e539dfdae48161
pungpeee <pungpeekiku@gmail.com>
2021-04-14 23:58:41 +0700

    Add profile page

---
+++ src/components/basecard/BaseCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="shadow-xl rounded-lg p-2 ring-4 ring-pink-200 ring-inset">
+    <slot> </slot>
+  </div>
+</template>
---
+++ src/views/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="profilepage w-full">
+    <div class="w-full flex justify-center">
+      <h1 class="font-semibold text-lg m-3 text-yellow-900">
+        ･ ͜ʖ ･🥛 Pungpeee's Profile ˚◞🩰⟢
+      </h1>
+    </div>
+  </div>
+
+  <div class="text-lg w-full flex flex-col items-center justfiy-center">
+    <div class="w-80 flex justify-center ">
+      <img :src="img"/>
+    </div>
+    <base-card class="mt-3">
+      <p>🍭Name : {{ name }}</p>
+      <p>🍭Birthday : {{ birth }}</p>
+      <p>🍭Youtube : {{ youtube }}</p>
+      <p>🍭Facebook : {{ fb }}</p>
+    </base-card>
+  </div>
+</template>
+<script>
+import BaseCard from "../components/basecard/BaseCard.vue";
+export default {
+  components: { BaseCard },
+  data() {
+    return {
+      name: "Wanisara Khamracha",
+      birth: "19 July 2001",
+      youtube: "Pungpeee",
+      fb: "Pond Waniizarah",
+      img: require("../assets/me.jpg"),
+    };
+  },
+};
+</script>
