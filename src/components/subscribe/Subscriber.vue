c89f4e362c980f67098aa780ce18bde1f57227af
pungpeee <pungpeekiku@gmail.com>
2021-04-18 22:05:01 +0700

    62130500079 Decorate edit and delete icon

--- src/components/subscribe/Subscriber.vue
+++ src/components/subscribe/Subscriber.vue
@@ -1,16 +1,15 @@
 <template>
-  <h1 class="border-pink-500 rounded-xl shadow p-2 my-2">{{ name }} like Pungpeee {{ like }} <i class="far fa-edit" @click="editbtn">
-    </i>   
-  <i class="far fa-trash-alt" @click="deletebtn"> </i>
+  <h1 class="border-pink-500 rounded-xl shadow p-2 my-2">
+    {{ name }} like Pungpeee {{ like }}
+    <i class="far fa-edit pointer" @click="editbtn"> </i>   <i class="far fa-trash-alt pointer" @click="deletebtn"> </i>
   </h1>
-  
 </template>
 
 <script>
 export default {
   name: "Subscribers",
   props: ["name", "like", "id"],
-  
+
   methods: {
     editbtn() {
       this.$emit("editevent", {
@@ -19,16 +18,17 @@ export default {
         id: this.id,
       });
     },
-deletebtn() {
+    deletebtn() {
       this.$emit("deleteevent", {
         id: this.id,
       });
     },
-  }
-
-  }
-  
+  },
+};
 </script>
 
 <style>
+.pointer {
+  cursor: pointer;
+}
 </style>
